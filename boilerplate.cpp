9c9290ec67210f595f059188a6bffec15ef6d4ab
Manish Kumar Sharma <105535356+shubhman20@users.noreply.github.com>
2023-02-09 14:09:13 +0530

    boilerplate

---
+++ boilerplate.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define ll long long
+
+int main(){
+	#define fast ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+
+	}
+}
